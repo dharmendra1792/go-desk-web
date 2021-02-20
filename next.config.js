ab78e0e17bd115f1a9dda0ac40b4b32e53ab26e2
Dharmendra Yaduwanshi <dharmendra@bellwether.org.in>
2021-02-20 16:18:25 +0530

    next.config updated & unwanted file deleted

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     MONGO_URI:
       "mongodb+srv://dharam_user:ksfkHESM7RZzCf5x@cluster0.fmhqa.mongodb.net/GoDesk?retryWrites=true&w=majority",
-    API_URL: "http://localhost:3000",
+    // API_URL: "http://localhost:3000",
+    API_URL: "https://go-desk.herokuapp.com",
   },
 };
